d7de1a923bb54199e7af19a15393738d524afe8c
AbbyFraenkel <72453241+AbbyFraenkel@users.noreply.github.com>
2024-09-01 21:09:51 +0200

    simplified

--- src/KitchenSink.jl
+++ src/KitchenSink.jl
@@ -4,7 +4,6 @@ using LinearAlgebra, SparseArrays, StaticArrays, JuMP, Ipopt, IterativeSolvers
 using Plots, ColorSchemes, LaTeXStrings, Documenter, DocStringExtensions, AlgebraicMultigrid
 using FastGaussQuadrature, IncompleteLU
 
-
 include("KSTypes/KSTypes.jl")
 include("CoordinateSystems/CoordinateSystems.jl")
 include("SpectralMethods/SpectralMethods.jl")
@@ -16,21 +15,21 @@ include("SpectralMethods/SpectralMethods.jl")
 # include("Preprocessing/Preprocessing.jl")
 # include("AdaptiveMethods/AdaptiveMethods.jl")
 # include("MultiLevelMethods/MultiLevelMethods.jl")
-# # # Need to update
+
 # include("Preconditioners/Preconditioners.jl")
-# # # Need to update
+
 # include("LinearSolvers/LinearSolvers.jl")
 
 # include("ProblemTypes/ProblemTypes.jl")
-# # # Need to update
+
 # include("TimeStepping/TimeStepping.jl")
-# # # Need to update
+
 # include("Optimization/Optimization.jl")
 
 # include("BoundaryConditions/BoundaryConditions.jl")
 
 # include("DomainDecomposition/DomainDecomposition.jl")
-# # Need to update
+
 # include("Visualization/Visualization.jl")
 
 end # module KitchenSink
