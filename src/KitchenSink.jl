e0bcd69666775fff1fef30d982c96114846696e4
AbbyFraenkel <72453241+AbbyFraenkel@users.noreply.github.com>
2024-09-01 21:06:29 +0200

    bare bones version

--- src/KitchenSink.jl
+++ src/KitchenSink.jl
@@ -9,28 +9,28 @@ include("KSTypes/KSTypes.jl")
 include("CoordinateSystems/CoordinateSystems.jl")
 include("SpectralMethods/SpectralMethods.jl")
 
-include("CommonMethods/CommonMethods.jl")
-include("IntergridOperators/IntergridOperators.jl")
-include("ErrorEstimation/ErrorEstimation.jl")
+# include("CommonMethods/CommonMethods.jl")
+# include("IntergridOperators/IntergridOperators.jl")
+# include("ErrorEstimation/ErrorEstimation.jl")
 
-include("Preprocessing/Preprocessing.jl")
-include("AdaptiveMethods/AdaptiveMethods.jl")
-include("MultiLevelMethods/MultiLevelMethods.jl")
-# # Need to update
-include("Preconditioners/Preconditioners.jl")
-# # Need to update
-include("LinearSolvers/LinearSolvers.jl")
+# include("Preprocessing/Preprocessing.jl")
+# include("AdaptiveMethods/AdaptiveMethods.jl")
+# include("MultiLevelMethods/MultiLevelMethods.jl")
+# # # Need to update
+# include("Preconditioners/Preconditioners.jl")
+# # # Need to update
+# include("LinearSolvers/LinearSolvers.jl")
 
-include("ProblemTypes/ProblemTypes.jl")
-# # Need to update
-include("TimeStepping/TimeStepping.jl")
-# # Need to update
-include("Optimization/Optimization.jl")
+# include("ProblemTypes/ProblemTypes.jl")
+# # # Need to update
+# include("TimeStepping/TimeStepping.jl")
+# # # Need to update
+# include("Optimization/Optimization.jl")
 
-include("BoundaryConditions/BoundaryConditions.jl")
+# include("BoundaryConditions/BoundaryConditions.jl")
 
-include("DomainDecomposition/DomainDecomposition.jl")
-# Need to update
-include("Visualization/Visualization.jl")
+# include("DomainDecomposition/DomainDecomposition.jl")
+# # Need to update
+# include("Visualization/Visualization.jl")
 
 end # module KitchenSink
